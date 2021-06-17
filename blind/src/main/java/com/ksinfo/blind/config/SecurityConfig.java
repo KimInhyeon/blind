76db1f408d90b061a060fa489869a7f6804d7c94
changju.lee <changju.lee@ks-infosys.com>
2021-06-17 09:37:05 +0900

    企業管理ページ（作成中）

--- blind/src/main/java/com/ksinfo/blind/config/SecurityConfig.java
+++ blind/src/main/java/com/ksinfo/blind/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ksinfo.blind.config;
 
+import com.ksinfo.blind.security.CustomizeAuthenticationFailureHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,23 +21,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//		.antMatchers("/companyMain/**", "/topicMain/**").hasRole("RM")
-//		.antMatchers("/companyMain/**", "/topicMain/**").hasRole("NM")
-//		.antMatchers("/", "/main", "/registMember").permitAll()
-//		.anyRequest().authenticated()
-//		.and().csrf().disable()
-//	.formLogin()
-//		.loginPage("/login")
-//		.defaultSuccessUrl("/loginSuccess")
-//		.failureHandler(new CustomizeAuthenticationFailureHandler())
-//		.permitAll()
-//		.and()
-//	.logout()
-//		.permitAll()
-//		.and()
-//	.exceptionHandling()
-//		.accessDeniedPage("/login");
+		http.authorizeRequests()
+		.antMatchers("/companyMain/**", "/topicMain/**").hasRole("RM")
+		.antMatchers("/companyMain/**", "/topicMain/**").hasRole("NM")
+		.antMatchers("/", "/main", "/registMember").permitAll()
+		.anyRequest().authenticated()
+		.and().csrf().disable()
+	.formLogin()
+		.loginPage("/login")
+		.defaultSuccessUrl("/loginSuccess")
+		.failureHandler(new CustomizeAuthenticationFailureHandler())
+		.permitAll()
+		.and()
+	.logout()
+		.permitAll()
+		.and()
+	.exceptionHandling()
+		.accessDeniedPage("/login");
 	}
 
 	@Bean
